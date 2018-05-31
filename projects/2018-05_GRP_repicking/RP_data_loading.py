d0b045064b081a12abbd03c3cdc5c168ee2827ef
atilapaes <atila.paes@gmail.com>
2018-05-31 04:39:03 -0600

    RP_data_loading module
    Repicking project
    
    Phase 2: Module for MS data loading
    Starting on May 30 (with code from previous projects)
    
    Objective: Build a module to get the catalog from Phase 1, load raw
    data, filter and delivery a MS slice.
    (The idea is to delivery the data to be processes in the next step of
    ATP)
    Guidelines: Use as much legacy code (previous projects) as possible
    
    Step 1: Develop a function to extract the times from G.R.P catalog of
    events.
    Step 2: Develop a function that uses a date_time object to load the
    respective time slice from the raw dataset. Then apply a bandpass
    filter.

--- projects/2018-05_GRP_repicking/RP_data_loading.py
+++ projects/2018-05_GRP_repicking/RP_data_loading.py
@@ -8,33 +8,44 @@ Created on Tue May 29 02:17:55 2018
 Repicking project 
 
 Phase 2: Module for MS data loading
-Starting on May 30 (with legacy code)
+Starting on May 30 (with code from previous projects)
 
-Objective: Build a module to get the catalog from Phase 1, load raw data, filter and delivery a MS slice
+Objective: Build a module to get the catalog from Phase 1, load raw data, filter and delivery a MS slice. 
+(The idea is to delivery the data to be processes in the next step of ATP)
 Guidelines: Use as much legacy code (previous projects) as possible
 
 
-
-Part 1: Develop a function to create the whole table of files/time to load
-Part 2: 
-    Develop a function that uses this table to to to de loadind and everything else
+Step 1: Develop a function to extract the times from G.R.P catalog of events
+Step 2: Develop a function that uses a date_time object to load the respective time slice from the raw dataset. Then apply a bandpass filter.
         
 """
 
-#%% Step 0: Loading libraries, catalogs and peojrct definitions
+#%% Step 0: Loading libraries, catalogs and preproject definitions
 
-import pandas, numpy, obspy
+# Include path to all modules into shared_modules folder
+import sys
+sys.path.insert(0,'/Users/atilapaes/Documents/GitHub/OpenMicroseismicV2/shared_modules/')
 
+import pandas, obspy # General Libs
+
+import om_load_data, om_catalog # O.M. shared modules
+
+import project_files.project_info # Project info from nested folder
 
 obspy.core.utcdatetime.UTCDateTime.DEFAULT_PRECISION = 3 # Define time resolution as 0.001 seconds
 
+
+#%% Step 0.1: Load project catalogs and prepare it for quick use
 # Load catalog of file names and SP time
 catalog_event_n_time=pandas.read_csv('project_files/catalog_events_n_time.csv')
 
-# Load catalog of files and times from project
-#?????
+# Import CSV catalog contaning strs for file_names and starttimes. Convert starttime to OBSPY date_time class
+# Could be considered as Step 2.0
+catalog_time_n_files=pandas.read_csv('project_files/catalog_time_n_files.csv')
+catalog_time_n_files['date_time']=catalog_time_n_files['date_time'].apply(lambda x: obspy.core.utcdatetime.UTCDateTime(x))
 
-#%% Step 2: Pre process the catalog from G.R.P. and output the date time of event
+
+#%% Step 1: Pre process the catalog from G.R.P. and output the date time of event
 # OBS: This function was build to consult one event per time
 
 def event_date_time(catalog_event_n_time,event_index):
@@ -61,12 +72,20 @@ def event_date_time(catalog_event_n_time,event_index):
 
     return(date_time)
         
-#%% Step 3: Use the previously developed scripts to load the raw SGY files
-event_index=0
 
+#%% Setp 2: Load ms data
+# OBS: - Work with OBSPY datetime format only. Resolution 1 second
 
-#%% 
+def rp_data_loading(input_time,sec_before,sec_after,catalog_time_n_files,bandpass_freqs=[10,200]):
+    # Step 2.1: Get name(s) of file(s) to load
+    files2load=om_catalog.time2file_name(input_time,sec_before,sec_after,catalog_time_n_files)
 
-#%% Step 4: load, slice and retunr
+    # Step 2.2: Load, slice and filter data
+    ms_data=om_load_data.load_sequential_files(files2load, input_time, sec_before, sec_after,bandpass_freqs,folder=project_files.project_info.ms_file_path)
+    
+    return(ms_data)
 
+#%% Commands to test the function from Step 3. It is working so far.
 
+input_time=obspy.core.utcdatetime.UTCDateTime('2016-11-20T06:20:30')
+ms_data=rp_data_loading(input_time,sec_before=15,sec_after=10,catalog_time_n_files=catalog_time_n_files,bandpass_freqs=[10,200])
