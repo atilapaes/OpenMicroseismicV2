abedda043fab1af6f8b68ee442afd200b0ec406b
atilapaes <atila.paes@gmail.com>
2018-06-06 02:01:01 -0600

    Update kurt to OBSPY stream
    Includes the function kurd_nd that applies the kurt_1d to an OBSPY
    stream of n channels.

--- shared_modules/om_char_functions.py
+++ shared_modules/om_char_functions.py
@@ -11,24 +11,29 @@ import matplotlib.pyplot as plt
 from scipy import stats
 
 #%%
-def kurt_1d(signal,samples):
+def kurt_1d(signal_1d,samples):
     """
     Calculate the kurtosis for a 1-D signal
     """
     ###########################################################################
-    def kurt(index,signal,samples):
+    def kurt(index,signal_1d,samples):
     #Kurtosis of the signal at a particular index
         if index < samples:
             return(0)
         else:
-            return(stats.kurtosis(signal[index-samples:index], axis=0, fisher=True, bias=True))
+            return(stats.kurtosis(signal_1d[index-samples:index], axis=0, fisher=True, bias=True))
     ###########################################################################
-    kurt_signal=signal.copy()
-    for index in range(len(signal)):
-        kurt_signal[index]=kurt(index,signal=signal,samples=samples)
+    kurt_signal=signal_1d.copy()
+    for index in range(len(signal_1d)):
+        kurt_signal[index]=kurt(index,signal_1d=signal_1d,samples=samples)
     return(kurt_signal)
 #%%
-
-
-
-    
+def kurt_nd(signal_nd, samples):
+    """
+    Apply the kurtosis function to all traces in a OBSPY stream
+    """
+    kurt_array=signal_nd.copy()
+    for index in range(len(signal_nd)):
+        kurt_array[index].data=kurt_1d(signal_1d=signal_nd[index].data,samples=samples)
+    return(kurt_array)
+#%%
